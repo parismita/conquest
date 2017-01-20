f180752129e726277cf671cf489b5a9a40d2e226
parismita <parismita@iitg.ernet.in>
2017-01-20 18:04:45 +0530

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-import conquest_main
-
+import conquest_main 
+from conquest_main import getThresoldValue,locateObstacle,resources,locateMap
 
 #tast
 #get frame size
@@ -16,23 +16,18 @@ import conquest_main
 
 #capture = cv2.VideoCapture(0)
 '''a=np.array([[[  1,   1]],
-
        [[  1, 478]],
-
        [[638, 478]],
-
        [[638,   1]]])'''
 
 ''''def computerHCF(x, y):
    """This function takes two
    integers and returns the H.C.F"""
-
    # choose the smaller number
    if x > y:
        smaller = y
    else:
        smaller = x
-
    for i in range(1,smaller + 1):
        if((x % i == 0) and (y % i == 0)):
            hcf = i
@@ -48,7 +43,9 @@ import conquest_main
 	print frame_size
 	cv2.waitKey(1)'''
 
-
+#############################################################################################3
+#                         A star algo
+#############################################################################################
 class Node:
     def __init__(self,value,point):
         self.value = value
@@ -124,40 +121,49 @@ def next_move(pacman,food,grid):
     for node in path:
         x, y = node.point
         print x, y
-#pacman_x, pacman_y = [ int(i) for i in raw_input().strip().split() ]
-#food_x, food_y = [ int(i) for i in raw_input().strip().split() ]
-#x,y = [ int(i) for i in raw_input().strip().split() ]
-############################################################################
 
+############################################################################
+############################################################################################3
+#print resources[0][1]
+
+oMin , oMax = getThresoldValue('obstacle')
+obstacles = locateObstacle(oMin , oMax)
+mMin , mMax = getThresoldValue('map')
+map1 = locateMap(mMin , mMax)
+#print map1,"map"
 #grid created with default value one
-grid = ones((120,120),dtype=int)
+grid = np.ones((120,120),dtype=int)
 #pixel cordinaten of map(3m*3m)
-map1=conquest_main.cntSet[0]
+#print map1
+########!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!not yet threshonded and founf contour of the arena 
 #pixel lenght of map
 map_y=map1[0][0][1]-map1[1][0][1]
-map_x=map1[3][0][0]-map1[0][0][0]
+map_x=map1[2][0][0]-map1[0][0][0]
+
 #dimension of each block of grid in terms of pixels... its in float
 pixels_per_block=[map_x/120.0,map_y/120.0]
 cordinates_obstacle=[[]]
 #coordinates of obstacles....marking there boxes in grid with '%'
-for x in conquest_main.square:
-	for i in range(4):
-		a=int((x[i][0][0]-map1[0][0][0])/pixels_per_block[0])
-		b=int((x[i][0][1]-map1[0][0][1])/pixels_per_block[1])
-		cordinates_obstacle.append([a,b])
-		grid[a][b]='%'
+for x in obstacles:
+    print x,x.shape
+    for i in range(x.shape[0]):
+        a=int((x[i][0][0]-map1[0][0][0])/pixels_per_block[0])
+        b=int((x[i][0][1]-map1[0][0][1])/pixels_per_block[1])
+        cordinates_obstacle.append([a,b])
+        grid[a][b]=0
 cordinates_food=[]
 cordinates_wood=[]
 #finding the box for food and wood in the grid through there centroid..... cordinate will be automatically sorted
-for x,x_type in conquest_main.reslist:
-	if x_type=='f':
-		f_x=int((x[0]-map1[0][0][0])/pixels_per_block[0])
-		f_y=int((x[1]-map1[0][0][1])/pixels_per_block[1])
-		cordinates_food.append([f_x,f_y])
-	else:
-		w_x=int((x[0]-map1[0][0][0])/pixels_per_block[0])
-		w_y=int((x[1]-map1[0][0][1])/pixels_per_block[1])
-		cordinates_wood.append([w_x,w_y])
+print resources,resources.shape
+for x in resources:
+    if x[1]==1:
+        f_x=int((x[0]-map1[0][0][0])/pixels_per_block[0])
+        f_y=int((x[1]-map1[0][0][1])/pixels_per_block[1])
+        cordinates_food.append([f_x,f_y])
+    else:
+        w_x=int((x[0]-map1[0][0][0])/pixels_per_block[0])
+        w_y=int((x[1]-map1[0][0][1])/pixels_per_block[1])
+        cordinates_wood.append([w_x,w_y])
 #town_center coordinates
 town_center[0]=int((tcCenter[0]-map1[0][0][0])/pixels_per_block[0])
 town_center[1]=int((tcCenter[1]-map1[0][0][1])/pixels_per_block[1])
@@ -172,3 +178,47 @@ town_center[1]=int((tcCenter[1]-map1[0][0][1])/pixels_per_block[1])
     #grid.append(list(raw_input().strip()))
  
 next_move(town_center,goal, grid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#how to make contours line show, how to make grid show
+def listPathPoints1(start,end):
+    stx=start[0]
+    sty=start[1]
+    enx=end[0]
+    eny=end[1]
+    l=[]
+    vx=(enx-stx)
+    vy=(eny-sty)
+    if (vx==0 and vy==0):
+        return start
+    d=int(math.sqrt((stx-enx)**2+(sty-eny)**2))
+    cos=vx/math.sqrt(vx**2+vy**2)
+    sin=vy/math.sqrt(vx**2+vy**2)
+    i=1
+    while(i<d):
+        l.append([int(stx+cos*i),int(sty+sin*i)])
+        i=i+1
+    return l
+
+
+
+
+
